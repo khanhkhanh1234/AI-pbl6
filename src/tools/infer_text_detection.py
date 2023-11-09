5c2cd762cb5912da2b7177c649dd9219432aed5a
Quoc Khanh <nguyenkimt322@gmail.com>
2023-11-09 22:46:48 +0700

    fix bug

--- src/tools/infer_text_detection.py
+++ src/tools/infer_text_detection.py
@@ -1,7 +1,7 @@
 from modules.text_detection import TextDetection    
 import cv2
 
-IMG_PATH = r"samples/inner_right.jpg"
+IMG_PATH = r"samples/front.png"
 
 if __name__ == "__main__":
     detector = TextDetection(device="cpu")
@@ -11,10 +11,15 @@ if __name__ == "__main__":
     image = image[:, :, ::-1]
     clone_image = image.copy()
 
+    bounding_box_count = 0  # Đếm số lượng bounding box
+
     for i, line in enumerate(detector(image)):
         line_text = ""
         for bbox, image in line:
             clone_image = cv2.polylines(clone_image, [bbox], isClosed=True, color=(255, 0, 0), thickness=1)
-            
+            bounding_box_count += 1
+    
+    print(f"Số lượng bounding box được nhận diện: {bounding_box_count}")
+
     cv2.imshow("result", clone_image)
     cv2.waitKey(0)
